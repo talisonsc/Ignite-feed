99c4c7b19c3a0797cb32d94d41a0fdd80c155654
talisonsc <talisonsilva8544@gmail.com>
2024-06-30 12:15:57 -0300

    sidebar-finished

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,6 +5,7 @@ console.log(igniteLogo)
 export function Header() {
   return (
     <header className={styles.header}>
+      {" "}
       <img src={igniteLogo} alt="Logotipo do ignite" />
     </header>
   )
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,4 +1,6 @@
+import { PencilLine } from "phosphor-react"
 import styles from "./sidebar.module.css"
+
 export function Sidebar() {
   return (
     <asside className={styles.sidebar}>
@@ -8,12 +10,15 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://lh3.googleusercontent.com/pw/AP1GczNZGNecFvvvhL07pKsZxutgVpGVlARnCLAYXhF3GofrovRCp3Z7lBssjXBb2GMcB91y11dQPUD1NOaojb-8IgzwIb2vmPKAHyUKrTzko1xKlNu_Q0z3CKC03-iIBRUsXDCjLps6-f6YOLcDAIjhyxUrVQP6OqwQbejH2GSeBjrr8Wz2N9vgE_Cflid_Hg-8qYCNqvHP3MmIKWpBaflSlEKNiE-S12biWCGeBmTu1KrkjPkI9sn0FEfxIdU8U7b7sTYUEyzxgyuOQbPHqfcppbl_dHXQ04xolT1UFlMVZFFISfd30lkfazLtOnMDkAvJYlFLijfvYeqC05LrJNuRrgp3-gWWTRGb22Miot9cp8J5SIa9BXvFqMhQcEY2MKtF7yzGc_MBs95Xpc7n2OZYlqNjdSWtPrgAwW574xf4w7h19-w8gZdLtP6PChVqpLecY9ODa8L0EDvM8kDJZmJpUNnHuyWO4P1rYouXeSIAnb0YZPpkbeohnwzPJa6Yy1ULVwOz4YvJLGoDQMWNQWFoFgH8M5ioMgO07mMsOVT6pCH8Tv842w98ym-PBn6D3jzDSDoGAszGs2nr5AuQVCcJy1AEomkBlMYzcPZvhKmywjo7_AbIGkL33pZovsZQ54gRO7lPfNICQjBWPGXFLIU2lFXFLvNvXxR5xdvZdIxzltT4BsKatec_EFvTfY6NhVFiZNkOrusNgkutADytE1ojM5yYnxVCUj2z00wcHKa7r5UI8cPxziQtSezzVQCistwWpDjKTl4tM80xG6qV_JnMyLmIKl3fuurwEMxb57ZPORcnP0zHlVq1I29uQQGqQ8KEjbZg_NwljBFV4i8NN-SPx0I6T06chyoydN02_H7rfue_rojywex39fGc-VC2pMoJicGKNBDpS7tAhbXZKSJyKZTIXw=w500-h500-s-no-gm?authuser=0" />
+        <img className={styles.avatar} src="https://github.com/talisonsc.png" />
         <strong>Talison Silva</strong>
         <span>Web Developer</span>
       </div>
       <footer>
-        <a href="#">Editar seu Perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu Perfil
+        </a>
       </footer>
     </asside>
   )
